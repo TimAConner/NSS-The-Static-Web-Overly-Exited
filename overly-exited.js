55e3296a029b2809e7a9ecd5038a52187ee7aaa1
Tim A. Conner <timaconner1@gmail.com>
2017-11-29 16:17:40 -0600

    Refactored addExitment().

--- overly-exited.js
+++ overly-exited.js
@@ -3,13 +3,13 @@ var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the
 
 // The addExcitement function should accept the array as the sole argument
 function addExcitement (theWordArray) {
-    for(var i = 0; i < sentence.length; i++){
+    for(var i = 0; i < theWordArray.length; i++){
         document.getElementById("output").innerHTML+= "<p>";
         for(var x = 0; x <= i; x++){
             if((x+1) % 3 === 0){
-                document.getElementById("output").innerHTML += sentence[x] + "!".repeat(Math.round(x/3)) + " ";
+                document.getElementById("output").innerHTML += theWordArray[x] + "!".repeat(Math.round(x/3)) + " ";
             } else {
-            document.getElementById("output").innerHTML += sentence[x] + " ";
+            document.getElementById("output").innerHTML += theWordArray[x] + " ";
             }
         }
         document.getElementById("output").innerHTML+= "</p>";
